a1f1e6cc8144e37363f09cc9688610951f5dc445
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-08-11 09:07:27 +0530

    fixed some naming convention

--- employee_manager/src/main/java/io/suvadeep/employee_manager/exception/EmployeeNotFoundException.java
+++ employee_manager/src/main/java/io/suvadeep/employee_manager/exception/EmployeeNotFoundException.java
@@ -1,7 +1,7 @@
 package io.suvadeep.employee_manager.exception;
 
 public class EmployeeNotFoundException extends Exception {
-    public EmployeeNotFoundException(String messege) {
-        super(messege);
+    public EmployeeNotFoundException(String message) {
+        super(message);
     }
 }
